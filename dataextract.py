f9b74a404a3f3e281a0b8aa816ddd825ea1299de
chloe <risktechnocrat@gmail.com>
2014-06-25 22:33:20 -0400

    get data for all expiration dates and add 'symbol' and 'date' fields

--- dataextract.py
+++ dataextract.py
@@ -7,24 +7,24 @@ import re
 import pandas as pd
 import time
 import sys
+from time import strftime
 
+def getDbCollection():
 # connect to mongo
-connection = pymongo.Connection("mongodb://localhost", safe=True)
-print(connection.database_names())
+    connection = pymongo.Connection("mongodb://localhost", safe=True)
+    print(connection.database_names())
 
 # get a handle to the reddit database
-db=connection['optionstore']
-collection = db['option_chains']
-print(collection.count())
-print(db.collection_names())
+    db=connection['optionstore']
+    collection = db['option_chains']
+    print(collection.count())
+    print(db.collection_names())
+    return collection
 
-fname = 'rest.csv'
-my_data = pd.read_csv(fname, squeeze='true')
-
+'''
 prefix = 'http://www.google.com/finance/option_chain?q='
 suffix = '&output=json'
 
-'''
 urlstr = prefix + 'MSFT'+ suffix
 print urlstr
 option_data = urllib2.urlopen(urlstr)
@@ -35,25 +35,55 @@ print json_string
 #collection.insert(value)
 
 '''
-t0=time.clock()
-count=0
-for i, row in enumerate(my_data):
-    urlstr = prefix + row + suffix
-    print urlstr
-    try:
-        option_data = urllib2.urlopen(urlstr)
-        s = option_data.read()
-        json_string = re.sub(r'([a-zA-Z_]+):', r'"\1":', s)
-        print json_string
-        value = json.loads(json_string)
-        collection.insert(value)
-        count+=1
-    except:
-        print "Error:", sys.exc_info()[0]
-        print row, "oops no option data"
-        my_data=my_data.drop(i)
 
-my_data.to_csv('option_list.csv', index=False)
+def getJson(urlstr):
+    option_data = urllib2.urlopen(urlstr)
+    s = option_data.read()
+    json_string = re.sub(r'([a-zA-Z_]+):', r'"\1":', s)
+    #print json_string
+    value = json.loads(json_string)
+    return value
 
-print time.clock()-t0, "seconds process time"
-print 'Total optionable symbols: [{}]'.format(count)
+def insertAllJson(symbol, collection):
+    prefix = 'http://www.google.com/finance/option_chain?q='
+    suffix = '&output=json'
+    urlstr=prefix+symbol+suffix
+    frontMon = getJson(urlstr)
+
+    for date in frontMon["expirations"]:
+        exp_str='&expd='+`date['d']`+'&expm='+`date['m']`+'&expy='+`date['y']`
+        #print date['d'], date['m'], date['y']
+        urlstr_exp=prefix+symbol+exp_str+suffix
+
+        data = getJson(urlstr_exp)
+        data['symbol']=symbol
+        data['date']=strftime("%Y-%m-%d")
+        collection.insert(data)
+
+def run():
+    t0=time.clock()
+    my_data = pd.read_csv('symbols.csv', squeeze=True)
+
+    count=0
+    collection=getDbCollection()
+
+    for i, row in enumerate(my_data):
+
+        try:
+            insertAllJson(row,collection)
+            count+=1
+            print count, row
+        except:
+            print "Error:", sys.exc_info()[0]
+            print row, "oops no option data"
+            my_data=my_data.drop(i)
+
+    my_data.to_csv('option_list.csv', index=False)
+
+    print time.clock()-t0, "seconds process time"
+    print 'Total optionable symbols: [{}]'.format(count)
+
+if __name__ == '__main__':
+    run()
+
+
